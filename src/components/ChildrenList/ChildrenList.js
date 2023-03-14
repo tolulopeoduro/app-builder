88b2ccd8ce71d18e4a86e0a27a99dd627d899bc0
Tolu <tolulopeoduro345@gmail.com>
2023-03-14 22:44:54 +0100

    worked on the element tree

--- src/components/ChildrenList/ChildrenList.js
+++ src/components/ChildrenList/ChildrenList.js
@@ -9,7 +9,7 @@ export default () => {
 	return (
 		<div className={styles.container}>
 			<div>
-				<ChildrenItem element ={activeComponent?.name}/>
+				<ChildrenItem element ={activeComponent?.name} level = {0}/>
 				<button id="element_menu_button" className={element_menu ? styles.active : styles.inactive} onClick={()=> dispatch(element_menu ? set_element_menu(false) : set_element_menu(true))}>
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11 19v-6H5v-2h6V5h2v6h6v2h-6v6Z"/></svg>
 				</button>
