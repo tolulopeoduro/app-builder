61e1fb6e214392eb11db126465689dc51c5b2404
Tolu <tolulopeoduro345@gmail.com>
2023-06-28 23:31:03 +0100

    Started work on gradient background editng: finished linear gradient

--- src/components/BackgroundColor/BackgroundColor.js
+++ src/components/BackgroundColor/BackgroundColor.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styles from "./BackgroundColor.module.scss"
 import Dropdown from '../BottomBar/Dropdown/Dropdown'
 import Color from '../Color/Color'
@@ -14,6 +14,19 @@ const BackgroundColor = (props) => {
 	const change_color = (index, data) => {
 		let new_colors = [...colors]
 		new_colors.splice(index, 1, data);
+		change_value({colors : new_colors})
+	}
+
+	const add_to_gradient = (index) => {
+		const new_colors = [...colors]
+		const new_color = new_colors[index];
+		new_colors.splice(index, 0, new_color)
+		change_value({colors : new_colors})
+	}
+	
+	const remove_from_gradient = (index) => {
+		const new_colors = [...colors]
+		new_colors.splice(index, 1);
 		change_value({colors : new_colors})
 	}
 
@@ -40,8 +53,20 @@ const BackgroundColor = (props) => {
 			{
 				type.value === "gradient" &&
 				colors.map((color, index) => (
-					<Color type="background-color" initial_value={color} 
-					get_value={(data) => change_color(index, data)}/>
+					<div className={styles.color_item}>
+						<Color type="background-color" initial_value={color} 
+						get_value={(data) => change_color(index, data)}/>
+						<svg className={styles.button} xmlns="http://www.w3.org/2000/svg" 
+						onClick={() => add_to_gradient(index)}width="24" height="24" 
+						viewBox="0 0 24 24"><path fill="white" 
+						d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"/></svg>
+
+						<svg style={{width: "1.3rem", marginLeft: "0.25rem"}} 
+						onClick={() => remove_from_gradient(index)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+							<path fill="none" stroke="white" strokeLinecap="round" 
+							strokeLinejoin="round" strokeWidth="2" d="M20 5H9l-7 7l7 7h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2Zm-2 
+							4l-6 6m0-6l6 6"/></svg>
+					</div>
 				))
 			}
 		</div>
