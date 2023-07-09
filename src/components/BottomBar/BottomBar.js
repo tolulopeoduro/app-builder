5abea5649fb32e05045fc7c26917edaf79ac0a89
Tolu <tolulopeoduro345@gmail.com>
2023-07-09 22:19:49 +0100

    started work on text editing

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -25,11 +25,7 @@ const BottomBar = (props) => {
 		<motion.div initial={{top : window.innerHeight + 10}} animate={{opacity:1, top: "50%"}} exit={{ top : window.innerHeight + 10}} className = {styles.bottom_bar_container}>
 			<div className={styles.left}>
 				<div className = {styles.body}>
-					{
-						active_element?.tag !== "div" ? 
-						<TextEditor/> :
-						<ContainerEditor/>
-					}
+					<ContainerEditor/>
 				</div>
 				<div className = {styles.footer}>
 				</div>
